cb043bb24ccab4aea0d70ca1de658ff452ced058
renatodru <renatodruregis@gmail.com>
2022-04-12 16:27:04 -0300

    ok

---
+++ 35.py
@@ -0,0 +1,16 @@
+
+
+
+def comb(lista):
+    
+    
+    
+    
+    
+    
+    return listacomb
+    
+lista =[0,5,10,11,12,43,83,123,1234]
+
+for l in lista:
+    print(comb(lista))
---
+++ tst.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a,b,c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    printf("%d\n", a+b+c);
+
+    return 0;
+
+}
+
